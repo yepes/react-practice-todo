f944cf79ecd15916c7891d0e60f328e28c85b34c
Jesus Yepes <jesus@jesusyepes.com>
2019-06-07 21:01:38 +0200

    commit pendiente. Los todos se pueden guardar en local.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    const todos = JSON.parse(localStorage.getItem('todos')) || [];
+
+    this.state = {
+      todos
+    }
+  }
+
+  handleTitleChange = e => {
+    console.log(e);
+    console.log(e.target.keyCode);
+    this.setState({
+      title: e.target.value
+    });
+  };
+
+  addTodo = () => {
+    const todos = [...this.state.todos, {
+      id: 1 + Math.random(),
+      title: this.state.title
+    }];
+    this.setState({
+      todos,
+      title: ''
+    });
+    localStorage.setItem('todos', JSON.stringify(todos));
+  };
+
   render() {
+    const { todos } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {todos.length > 0 &&
+            <div className="todos">
+              <ul>
+              {todos.map((todo, index) => {
+                return <li key={todo.id}>{todo.title}</li>
+              })}
+              </ul>
+            </div>
+        }
+        <div className="addTodo">
+          <input
+              type="text"
+              value={this.state.title}
+              onChange={this.handleTitleChange}
+          />
+          <button onClick={this.addTodo}>Agregar</button>
+        </div>
       </div>
     );
   }
